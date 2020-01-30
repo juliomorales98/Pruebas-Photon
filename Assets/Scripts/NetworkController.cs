11a7d09d12d783f3a24ddd114312b1283ec05850
juliomorales98 <juliocesar.mr@protonmail.com>
2020-01-30 16:48:48 -0600

    Primer commit.
    La aplicación solamente se conecta al mejor servidor de Photon en ese momento y nos dice cuál es.

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks {
+
+	// Use this for initialization
+	void Start () {
+		//Nos conectamos al mejor servidor
+		PhotonNetwork.ConnectUsingSettings();
+	}
+	
+	public override void OnConnectedToMaster(){
+		//Nos dice a qué región estamos conectados
+		Debug.Log("Conectados a " + PhotonNetwork.CloudRegion + " server");
+	}
+}
