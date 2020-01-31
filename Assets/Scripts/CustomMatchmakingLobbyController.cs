b4afc449a044a9d5ed9058a6d7e7bd1663a0fd20
juliomorales98 <juliocesar.mr@protonmail.com>
2020-01-30 18:45:01 -0600

    Creamos lobby, rooms y conexión a una escena
    Se tienen los lobbys, rooms y carga la escena.
    Falta arreglas que los nombres de los jugadores aparezcan de forma correcta en el room y que cuando comienze el juego, los clientes no vean el menú del room todavía.

---
+++ Assets/Scripts/CustomMatchmakingLobbyController.cs
@@ -0,0 +1,131 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomMatchmakingLobbyController : MonoBehaviourPunCallbacks {
+
+	[SerializeField]
+	private GameObject lobbyConnectButton;//para conectarse al lobby
+
+	[SerializeField]
+	private GameObject lobbyPanel; //panel que se mostrará en el lobby
+
+	[SerializeField]
+	private GameObject mainPanel; //panel que se mostrará en el menú principal
+
+	[SerializeField]
+	private InputField playerNameInput;
+
+	private string roomName;
+	private int roomSize;
+
+	private List<RoomInfo> roomListings; //rooms actuales
+
+	[SerializeField]
+	private Transform roomsContainer; //contenedor para tener la lista de rooms
+
+	[SerializeField]
+	private GameObject roomListingPrefab;
+
+	public override void OnConnectedToMaster(){
+		lobbyConnectButton.SetActive(true);
+
+		roomListings = new List<RoomInfo>();
+
+		//validamos el nombre del jguador
+		if(PlayerPrefs.HasKey("NickName")){
+			if(PlayerPrefs.GetString("NickName") == ""){
+				//Le asignamos un nombre random
+				PhotonNetwork.NickName = "Player " + Random.Range(0, 1000);
+			}else{
+				PhotonNetwork.NickName = PlayerPrefs.GetString("NickName");
+			}
+		}else{
+			//Le asignamos un nombre random
+			PhotonNetwork.NickName = "Player " + Random.Range(0, 1000);
+		}
+
+		playerNameInput.text = PhotonNetwork.NickName; //Ponemos el nombre en el campo text
+	}
+
+	public void PlayerNameUpdate(string nameInput){
+
+		PhotonNetwork.NickName = nameInput;
+		PlayerPrefs.SetString("NickName", nameInput);
+	}
+
+	public void JoinLobbyOnClick(){
+		mainPanel.SetActive(false);
+		lobbyPanel.SetActive(true);
+		PhotonNetwork.JoinLobby();//Se intenta conectar a un room existente
+	}
+
+	//---------------------------Entramos en un lobby...........................\\
+
+	static System.Predicate<RoomInfo> ByName(string name){
+
+		return delegate (RoomInfo room){
+			return room.Name == name;
+		};
+	}
+
+	void ListRoom(RoomInfo room){
+		if(room.IsOpen && room.IsVisible){
+			GameObject tempListing = Instantiate(roomListingPrefab, roomsContainer);			
+			RoomButton tempButton = tempListing.GetComponent<RoomButton>(); //Script
+
+			tempButton.SetRoom(room.Name, room.MaxPlayers, room.PlayerCount);
+		}
+	}
+
+	public override void OnRoomListUpdate(List<RoomInfo> roomList){
+		int tempIndex;
+
+		foreach(RoomInfo room in roomList){
+
+			if(roomListings != null){
+				tempIndex = roomListings.FindIndex(ByName(room.Name));
+			}else{
+				tempIndex = -1;
+			}
+
+			if(tempIndex != -1){
+				roomListings.RemoveAt(tempIndex);
+				Destroy(roomsContainer.GetChild(tempIndex).gameObject);
+			}
+
+			if(room.PlayerCount > 0){
+				roomListings.Add(room);
+				ListRoom(room);
+			}
+		}
+	}
+
+	public void OnRoomNameChanged(string nameIn){
+		roomName = nameIn;
+	}
+
+	public void OnRoomSizeChanged(string sizeIn){
+		roomSize = int.Parse(sizeIn);
+	}
+
+	public void CreateRoom(){
+		Debug.Log("Creando room...");
+
+		RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)roomSize };
+		PhotonNetwork.CreateRoom(roomName, roomOps);
+	}
+
+	public override void OnCreateRoomFailed(short returnCode, string message){
+		Debug.Log("Error al crear room, intenta cambiar el nombre");
+	}
+
+	public void MatchmakingCancel(){
+		mainPanel.SetActive(true);
+		lobbyPanel.SetActive(false);
+		PhotonNetwork.LeaveLobby();
+	}
+
+}
---
+++ Assets/Scripts/CustomMatchmakingRoomController.cs
@@ -0,0 +1,108 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class CustomMatchmakingRoomController : MonoBehaviourPunCallbacks {
+
+	[SerializeField]
+	private int multiPlayerSceneIndex; //para cargar multiplayer scene
+
+	[SerializeField]
+	private GameObject lobbyPanel;
+
+	[SerializeField]
+	private GameObject roomPanel;
+
+	[SerializeField]
+	private GameObject startButton;
+
+	[SerializeField]
+	private Transform playersContainer;
+
+	[SerializeField]
+	private GameObject playerListingPrefab;
+
+	[SerializeField]
+	private Text roomNameDisplay;
+
+	void ClearPlayerListing(){
+
+		for( int i = playersContainer.childCount - 1; i >= 0; i--){
+			Destroy(playersContainer.GetChild(i).gameObject);
+		}
+
+	}
+
+	void ListPlayers(){
+		foreach(Player player in PhotonNetwork.PlayerList){
+			GameObject tempListing = Instantiate(playerListingPrefab, playersContainer);
+			Text tempText = tempListing.transform.GetChild(0).GetComponent<Text>();
+
+			tempText.text = player.NickName;
+
+		}
+	}
+
+	public override void OnJoinedRoom(){
+		roomPanel.SetActive(true);
+		lobbyPanel.SetActive(false);
+		roomNameDisplay.text = PhotonNetwork.CurrentRoom.Name;
+
+		//Si es host puede inicial el juego
+		if(PhotonNetwork.IsMasterClient){
+			startButton.SetActive(true);
+
+		}else{
+			startButton.SetActive(false);
+		}
+
+		ClearPlayerListing();
+		ListPlayers();
+	}
+
+	public override void OnPlayerEnteredRoom(Player newPlayer){
+		ClearPlayerListing();
+		ListPlayers();		
+	}
+
+	public override void OnPlayerLeftRoom(Player otherPlayer){
+		ClearPlayerListing();
+		ListPlayers();
+
+		//Host migration
+		//Validamos si el actual jugador se convirtió en host después de que este 
+		//abandonara el room
+		if(PhotonNetwork.IsMasterClient){
+			startButton.SetActive(true);
+		}
+	}
+
+	public void StartGame(){
+		if(PhotonNetwork.IsMasterClient){
+			PhotonNetwork.CurrentRoom.IsOpen = false; //Si está en false, jugadores ya no podrán unirse iniciado el juego
+			PhotonNetwork.LoadLevel(multiPlayerSceneIndex);
+		}
+	}
+
+	IEnumerator rejoinLobby(){
+		yield return new WaitForSeconds(1);
+		PhotonNetwork.JoinLobby();
+	}
+
+	public void BackOnClick(){
+		//Para evitar errores con el host al regresar al room
+
+		lobbyPanel.SetActive(true);
+		roomPanel.SetActive(false);
+
+		PhotonNetwork.LeaveRoom();
+		PhotonNetwork.LeaveLobby();
+
+		StartCoroutine(rejoinLobby());
+	}
+
+
+}
