a712950e74405d3d146659d0d106ec4b401441ab
juliomorales98 <juliocesar.mr@protonmail.com>
2020-02-14 10:21:19 -0600

    Avatar y GameObject de conexión ahora son diferentes.

--- Assets/Scripts/MultiplayerScene/GameSetupController.cs
+++ Assets/Scripts/MultiplayerScene/GameSetupController.cs
@@ -1,15 +1,21 @@
-﻿using Photon.Pun;
+﻿/*
+Inicializa el prefab para la conexión del servidor, no inicializa el avatar.
+*/
+
+using Photon.Pun;
 using System.IO;
 using UnityEngine;
 
 public class GameSetupController : MonoBehaviour {
+
+	
 
 	// Use this for initialization
 	void Start () {
 		CreatePlayer();
 	}
 	
-	// Update is called once per frame
+	
 	private void CreatePlayer(){
 		Debug.Log("Creando jugador");
 		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs","PhotonPlayer"),Vector3.zero, Quaternion.identity);
