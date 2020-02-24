64d942865bfe1029f8b92b238f334c615649108a
juliomorales98 <juliocesar.mr@protonmail.com>
2020-02-24 09:45:32 -0600

    Añadimos text en el menú para saber a qué región estamos conectados.

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkController : MonoBehaviourPunCallbacks {
 
+	[SerializeField] private Text serverText;
 	// Use this for initialization
 	void Start () {
 		//Nos conectamos al mejor servidor
@@ -15,5 +17,6 @@ public class NetworkController : MonoBehaviourPunCallbacks {
 		//Nos dice a qué región estamos conectados
 		Debug.Log("Conectados a " + PhotonNetwork.CloudRegion + " server");
 		PhotonNetwork.AutomaticallySyncScene = true;
+		serverText.text = "Conectado a " + PhotonNetwork.CloudRegion.ToString();
 	}
 }
